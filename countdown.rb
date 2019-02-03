4a50442fa72f292dcdf8a8e57ac26dd8080e3081
Logan Wohlers <lawohlers@gmail.com>
2019-02-03 22:08:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,12 @@ def countdown (num)
   return "HAPPY NEW YEAR!"
   
 end
+
+countdown_with_sleep(num)
+ while num!=0
+    puts "#{num} SECOND(S)!"
+    num-=1
+  end
+  return "HAPPY NEW YEAR!"
+  
+end
